9495d732ff58547019d45f770e9e37960d0f7353
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-05-10 17:43:11 +0200

    rotation kwadracik

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float rotateLeft = 0.111f;
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Quaternion rotationAmount = Quaternion.Euler(0, 0, rotateLeft);
+            this.gameObject.transform.rotation *= rotationAmount;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Quaternion rotationAmount = Quaternion.Euler(0, 0, -rotateLeft);
+            this.gameObject.transform.rotation *= rotationAmount;
+        }
     }
 }

b7d535fd7a727989952a5904b2961f8e96749189
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-05-10 21:16:46 +0200

    update strzelanie

--- Assets/Scripts/test/StrzelanieV2.cs
+++ Assets/Scripts/test/StrzelanieV2.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class Strzelaniev2 : MonoBehaviour
 {
     [SerializeField]
-    public GameObject projectilePrefab;
+    public GameObject projectilePrefab1, projectilePrefab2;
 
     private float nextShot = 0;
     private float nextShotTime = 0.3f;
+
+    private bool normalBullets = false;
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            normalBullets = !normalBullets;
+        }
+
         if (Time.time > nextShot)
         {
             nextShot = Time.time + nextShotTime;
-            Instantiate(projectilePrefab, transform.position, transform.rotation);
+            Instantiate(normalBullets ? projectilePrefab1 : projectilePrefab2, transform.position, transform.rotation);
         }
     }
 }

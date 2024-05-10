1fb5097922025e8a6984a7aee8ebec1628901e13
kuba11g <kubagorzadek@gmail.com>
2024-05-10 18:57:25 +0200

    strzelanie kierunek

---
+++ Assets/Scripts/Pocisk1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocisk1 : MonoBehaviour
+{
+    [SerializeField]
+    public int bulletSpeed;
+
+    private void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += Time.fixedDeltaTime * bulletSpeed * -transform.up;
+    }
+}

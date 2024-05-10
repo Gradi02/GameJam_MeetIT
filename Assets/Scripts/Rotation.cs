01537d94f9fab29b3b3287e8133d5a33010328ae
kuba11g <kubagorzadek@gmail.com>
2024-05-10 18:27:24 +0200

    dzialajace pocisk itp

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    private float rotateLeft = 0.111f;
+    private float rotate = 0.25f;
 
     void Update()
     {
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            this.gameObject.transform.Rotate(0, 0, rotateLeft += 0.01f);
+            this.gameObject.transform.Rotate(0, 0, +rotate);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            this.gameObject.transform.Rotate(0, 0, rotateLeft -= 0.01f);
+            this.gameObject.transform.Rotate(0, 0, -rotate);
         }
     }
 }

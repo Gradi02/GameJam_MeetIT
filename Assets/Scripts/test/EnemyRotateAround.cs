e38b067048a07f4336edc778dd4347f32085c4d1
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-05-10 22:21:58 +0200

    przeciwnik dookola

---
+++ Assets/Scripts/test/EnemyRotateAround.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyRotateAround : MonoBehaviour
+{
+    public GameObject RotateAnchor;
+    public GameObject Enemy;
+    void Start()
+    {
+        Enemy.transform.localScale = new Vector3(0, 0, 0);
+        int rotate = Random.Range(0, 360);
+        this.gameObject.transform.Rotate(0, 0, +rotate);
+        LeanTween.scale(Enemy, new Vector3(1, 1, 1), 0.5f).setEase(LeanTweenType.easeInOutSine);
+        LeanTween.rotateAround(RotateAnchor, new Vector3(0, 0, 1), 360f, 5f).setLoopCount(-1);
+        StartCoroutine(SpawnAround());
+    }
+
+    private IEnumerator SpawnAround()
+    {
+        int RandomSleep = Random.Range(20, 25);
+        yield return new WaitForSeconds(RandomSleep);
+        LeanTween.scale(Enemy, new Vector3(0,0,0), 0.5f).setEase(LeanTweenType.easeInOutSine);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/test/ShootToMiddle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShootToMiddle : MonoBehaviour
+{
+    public int cooldown = 2;
+    public GameObject bullet;
+
+    private void Start()
+    {
+        StartCoroutine(Shooting());
+    }
+
+    private IEnumerator Shooting()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cooldown);
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+    }
+}

960dd14a4682709aa6caf82226a88e28dc730254
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-05-11 11:54:02 +0200

    upd

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     private float rotate = 4f;
+    Animator animator;
 
     private void Start()
     {
-        Animator animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
     void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(0, 0, +rotate);
-            animator.SetBool("nazwaParametruBool", true);
+            animator.SetBool("swim", true);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(0, 0, -rotate);
+            animator.SetBool("swim", true);
         }
+        animator.SetBool("swim", false);
     }
 
     public void IncrsRot()

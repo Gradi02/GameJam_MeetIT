c7319437fdfb18f7e230dd8a1657ef8ef78a12ed
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-05-11 06:43:46 +0200

    audio

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -7,9 +7,25 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject img;
+    private void Start()
+    {
+        img.SetActive(false);
+    }
+
+    [ContextMenu("t")]
     public void Gameover()
     {
         EdgeGlowVolume eg = VolumeManager.instance.stack.GetComponent<EdgeGlowVolume>();
         eg.Active = new BoolParameter(true, true);
+        FindObjectOfType<AudioManager>().Play("gameover");
+        StartCoroutine(end());
+    }
+
+    private IEnumerator end()
+    {
+        yield return new WaitForSeconds(2f);
+        Time.timeScale = 0;
+        img.SetActive(true);
     }
 }

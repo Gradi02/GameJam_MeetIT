c05d60346b552da47ac98485f8cd062ca225da1f
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-05-11 13:23:42 +0200

    upd

---
+++ Assets/Prefabs/macka.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class macka : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.CompareTag("bullet"))
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<Kraken>().Damage();
+                Destroy(collision.gameObject);
+                FindObjectOfType<AudioManager>().Play("enemydeath");
+            }
+        }
+    }
+}
--- Assets/Scripts/Kraken.cs
+++ Assets/Scripts/Kraken.cs
@@ -20,15 +20,28 @@ public class Kraken : MonoBehaviour
     private int krakenHealth = 10000;
     
     public Slider krakenHP;
+    private float nextShot = 0;
+    private float nextShotTime = 5f;
+    private bool boss = false;
+
+    public GameObject[] items;
+
+    private void Start()
+    {
+        krakenHP.gameObject.SetActive(false);
+    }
 
     [ContextMenu("kraken")]
     public void SpawnKraken()
     {
+        krakenHP.gameObject.SetActive(true);
         GetComponent<EnemySpawner>().enabled = false;
 
         StartCoroutine(spawn());
         krakenHP.maxValue = krakenHealth;
         krakenHP.minValue = 0;
+        nextShot = Time.time + nextShotTime;
+        boss = true;
     }
 
     private IEnumerator spawn()
@@ -45,6 +58,60 @@ public class Kraken : MonoBehaviour
 
     private void Update()
     {
-        //krakenHP.value = 
+        krakenHP.value = krakenHealth;
+
+        if(Time.time > nextShot && boss)
+        {
+            if(krakenHealth > 5000)
+            {
+                nextShot = Time.time + nextShotTime;
+                int r = Random.Range(0, 6);
+                MackaAtak(r);
+            }
+            else
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    nextShot = Time.time + nextShotTime;
+
+                    HashSet<int> drawnNumbers = new HashSet<int>();
+                    System.Random random = new System.Random();
+
+                    while (drawnNumbers.Count < 3)
+                    {
+                        drawnNumbers.Add(random.Next(0, 6));
+                    }
+
+                    foreach (int number in drawnNumbers)
+                    {
+                        MackaAtak(number);
+                    }
+                }
+            }
+        }
+
+        if(krakenHealth <= 0)
+        {
+            boss = false;
+            foreach(GameObject g in macki)
+            {
+                Destroy(g);
+            }
+            GetComponent<EnemySpawner>().enabled = true;
+            krakenHP.gameObject.SetActive(false);
+            Points.Score += 1000;
+            krakenHealth = 10000;
+        }
+    }
+
+    private void MackaAtak(int i)
+    {
+        GameObject g = items[Random.Range(0, items.Length)];
+        GameObject g2 = Instantiate(g, mackiPos[i], Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        krakenHealth -= 50;
     }
 }
